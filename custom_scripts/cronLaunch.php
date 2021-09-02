daeb884a611cd73790ba6df063a3ced7c98207ef
Julien Loizelet <julienloizelet@okaeli.com>
2021-09-02 11:56:40 +0900

    feat(cron): Add script and command to launch specific cron job from browser

---
+++ custom_scripts/cronLaunch.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ * Run a specific cron job from browser (development only, do not use in production)
+ * @see https://magento.stackexchange.com/a/273142/50208
+ * @example : https://my.magento2.site/cronLaunch.php?job=VendorName\ModuleName\CronClass
+ *
+ *
+ */
+use Magento\Framework\App\Bootstrap;
+
+require '../app/bootstrap.php';
+
+if (php_sapi_name() !== 'cli' && isset($_GET['job'])) {
+    define('CRONJOBCLASS', $_GET['job']);
+} elseif (php_sapi_name() !== 'cli') {
+    die('Please add the class of the cron job you want to execute as a job parameter (?job=Vendor\Module\Class)');
+} elseif (!isset($argv[1])) {
+    die('Please add the class of the cron job you want to execute enclosed IN DOUBLE QUOTES as a parameter.' . PHP_EOL);
+} else {
+    define('CRONJOBCLASS', $argv[1]);
+}
+
+class CronRunner extends \Magento\Framework\App\Http
+    implements \Magento\Framework\AppInterface
+{
+
+    public function __construct(
+        \Magento\Framework\App\State $state,\Magento\Framework\App\Response\Http $response)
+    {
+        $this->_response = $response;
+        $state->setAreaCode('adminhtml');
+    }
+
+    function launch()
+    {
+        $cron = \Magento\Framework\App\ObjectManager::getInstance()
+            ->create(CRONJOBCLASS);
+
+        $cron->execute();
+        return $this->_response;
+    }
+}
+
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+$app = $bootstrap->createApplication('CronRunner');
+$bootstrap->run($app);
