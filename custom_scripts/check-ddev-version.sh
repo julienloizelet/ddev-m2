017efb631ac1b2f17db7d6e7906c1b75b78f2f01
Julien Loizelet <julienloizelet@okaeli.com>
2022-08-25 18:42:43 +0900

    feat(DDEV version): Add files to handle DDEV expected version

---
+++ custom_scripts/check-ddev-version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Check DDEV version
+CURRENT_DIR=$(dirname "$0");
+source "${CURRENT_DIR}/bash/helpers/colors.sh";
+
+
+function show_banner() {
+    echo -e "${bldblu}=========================================================="
+    echo -e "> Magento 2 DDEV environment started successfully. "
+    echo -e "==========================================================${txtrst}"
+}
+show_banner
+cd ~
+CURRENT=$(printenv | grep -E "^DDEV_VERSION=v(.*)$" | sed 's/DDEV_VERSION=v//g')
+EXPECTED=$(cat DDEV_EXPECTED_VERSION.txt)
+
+if [[ $CURRENT == $EXPECTED ]]
+then
+  echo -en "${txtgrn}${check_mark} DDEV version is as expected: $CURRENT ${txtrst} \n"
+else
+  echo -e "${txtred}=========================="
+  echo -e "> WARNING!!!"
+  echo -e "==========================${txtrst}"
+  echo -en "${txtred}${cross_mark} DDEV version ($CURRENT) is not the expected one: $EXPECTED ${txtrst} \n"
+fi
